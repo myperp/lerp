5857c942f0e2ddfd9e32a5c3ff6af20a721a37b8
sexybee <achrafazhar@gmail.com>
2015-09-29 11:15:14 +0200

    Add usefull bins (bof).

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env sh
+
+echo ''
+echo '// Building development environment'
+
+rm -rf app/cache/* app/logs/*
+
+composer --quiet --no-interaction install --optimize-autoloader > /dev/null
+
+echo ''
+echo ' [OK] Development environment built'
+echo ''
